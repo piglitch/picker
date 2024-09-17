6db15aeb46a62d667871310dfd8a17690a6d3e2b
Avi <avibanerjee2000@gmail.com>
2024-09-17 18:26:13 +0530

    project name stylized

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -1,20 +1,23 @@
 import { useDispatch, useSelector } from "react-redux";
 import { decrement, increment } from "../redux/slices/counterSlice";
 import { RootState } from "../redux/store";
+import { useState } from "react";
 
 function Dashboard() {
+  const [status, setStatus] = useState();
   const count = useSelector((state: RootState) => state.counter.value);
   const dispatch = useDispatch();
-
   const fetchReq = async() => {  
     const res = await fetch("http://localhost:3000/", { mode: 'cors' });
     const data = await res.json();
-    console.log(data); 
+    setStatus(data.status)
+    console.log(data.status ? data.status : ''); 
   }
   fetchReq();
 
   return (
     <div className="content-format mt-64">
+      <p>Status: {status}</p>
       Counter: {count}
       <p/>
       <button className="bg-green-500 p-2 rounded-lg" onClick={() => dispatch(increment())}>+1</button>
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,10 +3,15 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="mx-auto flex h-24 max-w-7xl items-center justify-between gap-x-6 p-6 lg:px-8">
-      <div className="text-2xl my-auto"><Link to="/">picker.</Link></div>
+      <div className="text-2xl my-auto">
+        <Link to="/">
+          <span className="text-violet-500">pic</span>ker
+          <span className="text-yellow-400">.</span>
+        </Link>
+      </div>
       <div className="my-auto flex justify-between gap-6">
         <div><Link to="/dashboard">Dashboard</Link></div>
-       <div>SignIn</div> 
+        <div>SignIn</div> 
       </div>
     </nav>
   )
