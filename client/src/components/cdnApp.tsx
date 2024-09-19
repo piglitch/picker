55c2718aa228e35a10197fb02086b38686c82c9b
Avi <avibanerjee2000@gmail.com>
2024-09-20 01:42:29 +0530

    app view is built for each app

--- client/src/components/cdnApp.tsx
+++ client/src/components/cdnApp.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const CdnApp = () => {
   return (
-    <div className='content-format mt-6 flex'>
-      <div className='sidebar w-1/5 border'>
-        <div>Overview</div>
-        <div>Files</div>
-        <div>Settings</div>
+    <div className='content-format h-96 md:h-[720px] mt-6 flex border rounded-md bg-gray-200 text-black'>
+      <div className='sidebar flex flex-col gap-2 w-1/6'>
+        <div className='hover:bg-slate-300 p-1 w-48 rounded-md'>Overview</div>
+        <div className='hover:bg-slate-300 p-1 w-48 rounded-md'>Files</div>
+        <div className='hover:bg-slate-300 p-1 w-48 rounded-md'>Settings</div>
       </div>
-      <div className='w-4/5 border'>
+      <div className='w-5/6 border-l border-l-blue-400 px-6'>
         main section 
       </div>
     </div>
