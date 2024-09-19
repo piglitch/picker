bc0cf1dc2b4dc53871d7cffabae945316061cb38
Avi <avibanerjee2000@gmail.com>
2024-09-20 01:41:31 +0530

    using server data to send dummy data to client

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('/api/images')
+  @Get('/api/user-apps/')
   outputString(): object {
     //const sentence = `${this.appService.getHello()}, ${this.appService.sayHi()}`;
     return this.appService.sendJson();
