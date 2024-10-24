ddbeeb3125798915e94a82b7bf97b4bf2adc7fdd
Avi <avibanerjee2000@gmail.com>
2024-10-24 23:57:01 +0530

    react router links added instead of window relocation

--- client/src/components/ui/sideBar.tsx
+++ client/src/components/ui/sideBar.tsx
@@ -1,13 +1,26 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+
 
 const SideBar = () => {
   const params = useParams();
 
   return (
-    <div className='sidebar flex flex-col py-4 gap-2 w-1/6 mt-8'>
-      <div className='hover:bg-slate-300 p-1 w-48 rounded-md shadow-md' onClick={() => window.location.href = `/dashboard/app/${params.id}/overview/`}>Overview</div>
-      <div className='hover:bg-slate-300 p-1 w-48 rounded-md shadow-md' onClick={() => window.location.href = `/dashboard/app/${params.id}/files`}>Files</div>
-      <div className='hover:bg-slate-300 p-1 w-48 rounded-md shadow-md' onClick={() => window.location.href = `/dashboard/app/${params.id}/settings`}>Settings</div>
+    <div className='sidebar flex flex-col py-4 gap-2 w-1/6 mt-8'> 
+      <Link to={`/dashboard/app/${params.id}/overview/`}>
+        <div className='hover:bg-slate-300 p-1 w-48 rounded-md shadow-md'>
+          Dashboard  
+        </div>
+      </Link>
+      <Link to={`/dashboard/app/${params.id}/files/`}>
+        <div className='hover:bg-slate-300 p-1 w-48 rounded-md shadow-md'>
+          Files  
+        </div>  
+      </Link>
+      <Link to={`/dashboard/app/${params.id}/settings/`}>
+        <div className='hover:bg-slate-300 p-1 w-48 rounded-md shadow-md'>
+          Settings
+        </div>
+      </Link>      
     </div>
   )
 }
