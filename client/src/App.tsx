e799def961e937e44453e46bff690ac953f46729
Avi <avibanerjee2000@gmail.com>
2025-03-03 20:16:20 +0530

    removed unused junk

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,8 +2,6 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css'
 // import Dashboard from './components/Dashboard';
 import Home from './components/Home';
-import { Provider } from 'react-redux';
-import store from './redux/store';
 // import NewApp from './components/NewApp';
 import CdnAppOverview from './components/cdnAppOverview';
 import CdnAppFiles from './components/cdnAppFiles';
@@ -52,9 +50,7 @@ function App() {
     }
   ]);
   return (
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
+    <RouterProvider router={router} />
   )
 }
 
